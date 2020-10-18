475a0dbb30adb1729d6ca155685d0b3eff1c86ed
Piyush Kumar <piyush.midaas@gmail.com>
2020-10-18 18:18:13 +0530

    - Initialised DB with seed data - Persistant Volumes for reuse added - Postman Collection added

--- hapi-backend/dataScripts/13_10_2020/createreactionscollection.sh
+++
@@ -1,5 +0,0 @@
-# mongoimport --db test --collection inventory ^
-#           --authenticationDatabase admin --username <user> --password <password> ^
-#           --drop --file ~\downloads\inventory.crud.json --jsonArray
-
-mongoimport --db reddit-demo --collection reactiontypes --drop --file ./threeReactions.json --jsonArray
